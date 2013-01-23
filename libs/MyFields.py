919332a902ed7357cf4f0aa1f18ed2297ea3d445
CyaLiven <aliven.cen@gmail.com>
2013-01-23 21:34:47 +0800

    Update libs/MyFields.py

--- libs/MyFields.py
+++ libs/MyFields.py
@@ -5,14 +5,13 @@ from wtforms.fields import TextField
 from wtforms.widgets.core import TextArea
 
 class TextAreaField(TextField):
-    widget = TextArea()
 
     def __init__(self, width=100, high=100, *args, **kwargs):
         self.width = width
         self.high = high
-        super(TextAreaField,self).__init__(*args, **kwargs)
+        super(TextAreaField, self).__init__(*args, **kwargs)
 
     def __call__(self, **kwargs):
        kwargs['style'] = 'width: %spx; height: %spx;' %(self.width, self.high)
-       return self.widget(self, **kwargs)
+       super(TextAreaField, self).__call__(**kwargs)
 
